5d064fc7de226e2cb31a417275e8ecc58f3b262c
manana <manana@Core11-33>
2018-02-05 13:55:09 +0100

    Cambios ralizados

--- src/edu/goose/model/GestionUsuarios.java
+++ src/edu/goose/model/GestionUsuarios.java
@@ -1,5 +1,11 @@
 package edu.goose.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import edu.goose.beans.Usuario;
 
 public class GestionUsuarios {
@@ -13,15 +19,38 @@ public class GestionUsuarios {
 	 * @return true si el usuario es correcto, false si no lo es.
 	 */
 	public static boolean validar(String nombre, String password) {
-		/**TODO*/
 		if(nombre.equals("nombre"))
 			return true;
 		return false;
 	}
 	
 	
-	public static Usuario getUsuario() {
-		return null;
+	public static Usuario getUsuario(String nombre, String password) {
+		Usuario contacto = null;
+		/*try (Connection connection = DriverManager.getConnection(url_bd, user, pass)) {
+			// Intrucción SQL
+			String sql = "Select  * from contactos where nombre = ? and password = ?";
+			// Creo el PreparedStatement y lo ejecuto
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, nombre);
+			preparedStatement.setString(1, password);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				contacto = new Usuario(				
+						resultSet.getInt("id"),
+						resultSet.getString ("alias"),
+						resultSet.getString("nombre"),
+						resultSet.getString("apellido1"),
+						resultSet.getString("apellido2"),
+						resultSet.getString("email"),
+						resultSet.getInt("telefono"),
+						resultSet.getString("pais"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}*/
+		return contacto;
+		
 	}
 	
 }
